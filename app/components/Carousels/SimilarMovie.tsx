63fbfd9697cc210498ed020cb99c337bce0895c6
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-02-19 21:23:28 -0300

    Responsive detail page

---
+++ app/components/Carousels/SimilarMovie.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Carousel from '../Carousel'
+import Card from '../Card'
+import { SwiperSlide } from 'swiper/react'
+import { apiTypes } from 'interfaces/movie'
+import { useSimilarMovie } from '@/hooks/useSimilarMovie'
+
+export default function SimilarMovieCar({ id }) {
+  const { similar } = useSimilarMovie({ id })
+  return (
+    <Carousel>
+      {similar.map((props: apiTypes) => (
+        <SwiperSlide key={props.id}>
+          <Card
+            adult={props.adult}
+            id={props.id}
+            title={props.title}
+            poster_path={props.poster_path}
+            release_date={props.release_date}
+            popularity={props.popularity}
+          />
+        </SwiperSlide>
+      ))}
+    </Carousel>
+  )
+}
--- app/pages/detail/[id]/page.tsx
+++ app/pages/detail/[id]/page.tsx
@@ -1,22 +1,18 @@
 'use client'
+import SimilarMovieCar from '@/components/Carousels/SimilarMovie'
 import Image from 'next/image'
-import Carousel from '@/components/Carousel'
-import Card from '@/components/Card'
-import { SwiperSlide } from 'swiper/react'
 import { IoStar, IoStarHalf } from 'react-icons/io5'
 import { Rubik } from '@next/font/google'
-import { apiTypes } from '@/interfaces/movie'
 import { hoursConvert, useSingleMovie } from '@/hooks/useSingleMovie'
-import { useSimilarMovie } from '@/hooks/useSimilarMovie'
+import { BsDot } from 'react-icons/bs'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
 export default function Page({ params }) {
   const { singleMovie } = useSingleMovie({ id: params.id })
-  const { similar } = useSimilarMovie({ id: params.id })
 
   return (
-    <main className="w-full h-full">
+    <main className="w-full h-full overflow-hidden">
       <div className="w-full h-full fixed -z-10">
         <Image
           src={`https://image.tmdb.org/t/p/w500${singleMovie.poster_path}`}
@@ -25,17 +21,33 @@ export default function Page({ params }) {
         />
         <div className="backdrop-blur-md w-full h-full bg-black/50"></div>
       </div>
-      <div className="h-[100vh] max-w-[1200px] mx-auto p-4">
-        <div className="grid grid-cols-2 gap-5 place-items-center p-24">
-          <div className="grid gap-4">
+      <div className="h-[100vh] max-w-[1200px] mx-auto px-4">
+        <div className="grid md:grid-cols-2 gap-5 justify-center p-24 w-full">
+          <div className="w-[340px] h-auto md:w-[370px] overflow-hidden md:shadow-2xl shadow-black">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${singleMovie.poster_path}`}
+              alt={singleMovie.title}
+              width="370"
+              height="450"
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className="grid lg:flex lg:flex-col gap-4 lg:justify-center text-center lg:text-left place-items-center lg:place-items-start">
             <h1 className={`${rubik.className} text-4xl font-semibold`}>
               {singleMovie.title}
             </h1>
             <h1 className={`${rubik.className} text-sm`}>
               {singleMovie.overview}
             </h1>
-            <h1 className="text-lg">{hoursConvert(singleMovie.runtime)}</h1>
-            <h1 className="text-sm">{singleMovie.release_date}</h1>
+            <div className="inline-flex items-center gap-1">
+              <h1 className="text-sm">{singleMovie.release_date}</h1>
+              <BsDot className="w-7 h-auto" />
+              <h1 className="text-sm">Action, Fantasy</h1>
+              <BsDot className="w-7 h-auto" />
+              <h1 className="text-sm lg:text-lg">
+                {hoursConvert(singleMovie.runtime)}
+              </h1>
+            </div>
             <div className="inline-flex items-center gap-1">
               <IoStar className="fill-yellow-500" />
               <IoStar className="fill-yellow-500" />
@@ -47,35 +59,13 @@ export default function Page({ params }) {
               Watch
             </button>
           </div>
-          <div className="w-[370px] h-[520px] overflow-hidden shadow-2xl shadow-black">
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${singleMovie.poster_path}`}
-              alt={singleMovie.title}
-              width="370"
-              height="450"
-              style={{ objectFit: 'cover' }}
-            />
-          </div>
         </div>
       </div>
-      <div className="grid w-full bg-gradient-to-t from-black/90 to-black/0">
+      <div className="grid w-full mt-64">
         <h1 className={`${rubik.className} text-xl px-10 font-semibold`}>
           Similar movies
         </h1>
-        <Carousel>
-          {similar.map((props: apiTypes) => (
-            <SwiperSlide key={props.id}>
-              <Card
-                adult={props.adult}
-                id={props.id}
-                title={props.title}
-                poster_path={props.poster_path}
-                release_date={props.release_date}
-                popularity={props.popularity}
-              />
-            </SwiperSlide>
-          ))}
-        </Carousel>
+        <SimilarMovieCar id={params.id} />
       </div>
     </main>
   )
