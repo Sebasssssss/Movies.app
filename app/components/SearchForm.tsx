657f3961fd977bfded8fe33e6b4b25af5c6718a8
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-02-21 20:42:16 -0300

    Updates

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -29,7 +29,7 @@ export default function SearchForm() {
         onChange={handleChange}
         value={keyword}
         ref={ref}
-        className="bg-transparent border-2 border-[#58586a] text-[#58586a] hover:border-[#EB454F] hover:shadow-search focus:shadow-search focus:border-[#EB454F] w-[150px] focus:w-[250px] focus:bg-zinc-900 focus:text-white rounded-full px-4 py-1 outline-none placeholder:text-[#58586a] transition-all duration-200"
+        className="bg-transparent border-2 border-[#58586a] text-[#58586a] hover:border-[#EB454F]/60 hover:shadow-search focus:shadow-search focus:border-[#EB454F] w-[150px] focus:w-[250px] focus:bg-zinc-900 focus:text-white rounded-full px-4 py-1 outline-none placeholder:text-[#58586a] transition-all duration-200"
       />
       <button type="submit" className="absolute right-3">
         <FiSearch className="text-[#58586a] hover:text-[#EB454F] transition duration-200" />
